d51a7299d60a50c8a535424e65fb132642c59f65
Вячеслав Коханный <xoxluwa1@yandex.com>
2020-08-04 22:00:46 +0500

    fix: исправить HTML и CSS, оптимизировать javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,41 @@
 let popup = document.querySelector('.popup');
 
 let profileName = document.querySelector('.profile__name');
+
 let profileDescription = document.querySelector('.profile__description');
 
-let popupUserName = popup.querySelector('.popup__item_el_username');
-let popupActivity = popup.querySelector('.popup__item_el_activity');
+let popupUserName = popup.querySelector('.popup__item_element_username');
 
-popupUserName.value = profileName.textContent;
-popupActivity.value = profileDescription.textContent;
+let popupActivity = popup.querySelector('.popup__item_element_activity');
 
-//Открытие попапа
 let editButton = document.querySelector('.profile__edit-btn');
 
-function openPopup() {
-  popup.classList.add('popup_opened');
-}
-
-editButton.addEventListener('click', openPopup);
-
-//Закрыть попапа
 let popupCloseButton = popup.querySelector('.popup__close-btn');
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
-}
-
-popupCloseButton.addEventListener('click', closePopup);
-
-//Измненение и сохранение информации о себе
 let saveButton = popup.querySelector('.popup__save-btn');
 
 let popupContainer = popup.querySelector('.popup__container');
 
+
+//Функция открытия и закрытия попапа
+function openClosePopup() {
+  if (popup.classList.contains('popup_opened') === false) {
+    popupUserName.value = profileName.textContent;
+    popupActivity.value = profileDescription.textContent;
+  }
+  popup.classList.toggle('popup_opened');
+}
+
+editButton.addEventListener('click', openClosePopup);
+
+popupCloseButton.addEventListener('click', openClosePopup);
+
+//Измненение и сохранение информации о себе
 function popupContainerSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = popupUserName.value;
   profileDescription.textContent = popupActivity.value;
-  closePopup();
+  openClosePopup();
 }
 
 popupContainer.addEventListener('submit', popupContainerSubmitHandler);

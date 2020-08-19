54192c31210541bbfc7ebf09602defaffa2b41aa
Вячеслав Коханный <xoxluwa1@yandex.com>
2020-08-19 11:32:47 +0500

    feat: сделать так, чтобы пользователи могли добавлять свои фотографии мест

--- scripts/index.js
+++ scripts/index.js
@@ -25,47 +25,40 @@ const initialCards = [
   }
 ];
 
-let cardsListContainer = document.querySelector('.cards__list');
-
-let editProfile = document.querySelector('.edit-profile');
-
 let profile = document.querySelector('.profile');
 
 let profileName = profile.querySelector('.profile__name');
 
 let profileDescription = profile.querySelector('.profile__description');
 
-let profileEditButton = profile.querySelector('.profile__edit-btn');
+let editButton = profile.querySelector('.profile__edit-btn');
 
-let username = editProfile.querySelector('.username');
+let addPhotoButton = profile.querySelector('.profile__add-btn')
 
-let activity = editProfile.querySelector('.activity');
+let editProfile = document.querySelector('.edit-profile');
+
+let userForm = editProfile.querySelector('.user-form');
+
+let username = userForm.querySelector('.username');
+
+let activity = userForm.querySelector('.activity');
 
 let addPhoto = document.querySelector('.add-photo');
 
-let addPhotoButton = profile.querySelector('.profile__add-btn');
+let placeForm = addPhoto.querySelector('.place-form');
+
+let placeName = placeForm.querySelector('.place-name');
+
+let pictureLink = placeForm.querySelector('.picture-link')
 
 let popups = document.querySelectorAll('.popup');
 
+let cardsList = document.querySelector('.cards__list');
 
-//Функция открытия попапа для редактирования профиля
-let editProfileOpen = () => {
-  if (!editProfile.classList.contains('popup_opened')) {
-    username.value = profileName.textContent;
-    activity.value = profileDescription.textContent;
-  }
+let cardTemplate = document.querySelector('#cardItemTemplate').content;
 
-  editProfile.classList.add('popup_opened');
-};
-
-//Функция открытия попапа для добавления фото
-let addPhotoOpen = () => {
-  addPhoto.classList.add('popup_opened')
-};
-
-//Функция добавления фото из представленного массива при загрузке странице
-let addCardToContainer = item => {
-  let cardTemplate = document.querySelector('#cardItemTemplate').content;
+//Функция добавления фото из представленного массива при загрузке страницы
+let addCardToList = item => {
 
   let cardElement = cardTemplate.cloneNode(true);
 
@@ -73,61 +66,55 @@ let addCardToContainer = item => {
 
   cardElement.querySelector('.cards__image').src = item.link;
 
-  cardsListContainer.prepend(cardElement);
+  cardsList.prepend(cardElement);
 }
 
-//Открытие попапов
-document.addEventListener('click', (evt) => {
-  let elementTarget = evt.target;
-  if (elementTarget.classList.contains('profile__edit-btn')) {
-    editProfileOpen();
-  } else if (elementTarget.classList.contains('profile__add-btn')) {
-      addPhotoOpen();
-    }
-  return;
+initialCards.forEach(addCardToList);
+
+//Открытие попапа с формой для редактирования профиля
+editButton.addEventListener('click', () => {
+  if (!editProfile.classList.contains('popup_opened')) {
+    username.value = profileName.textContent;
+    activity.value = profileDescription.textContent;
+  }
+
+  editProfile.classList.add('popup_opened');
 });
 
-//Закрытие попапов
-popups.forEach((popup) => {
-  let popupClose = () => {
-    popup.classList.remove('popup_opened');
-  };
+//Открытие попапа с формой для добавления фотографии места
+addPhotoButton.addEventListener('click', () => {
+  addPhoto.classList.add('popup_opened');
+});
+
+//Закрытие каждого попапа
+popups.forEach(popup => {
   let closeButton = popup.querySelector('.popup__close-btn');
-  closeButton.addEventListener('click', popupClose);
+
+  closeButton.addEventListener('click', () => {
+    popup.classList.remove('popup_opened');
+  });
 });
 
-//Добавление фото при загрузке страницы
-initialCards.forEach(addCardToContainer);
-
-
-// let editProfile = document.querySelector('.popup_type_profile');
-
-// let profileName = document.querySelector('.profile__name');
-
-// let profileDescription = document.querySelector('.profile__description');
-
-// let userName = editProfile.querySelector('.popup__item_element_username');
-
-// let userActivity = editProfile.querySelector('.popup__item_element_activity');
-
-// let editButton = document.querySelector('.profile__edit-btn');
-
-// let popupCloseButton = editProfile.querySelector('.popup__close-btn');
-
-// let saveButton = editProfile.querySelector('.popup__save-btn');
-
-// let popupContainer = editProfile.querySelector('.popup__container');
-
-
-
-
-
-//Измненение и сохранение информации о себе
-function popupContainerSubmitHandler (evt) {
+//Изменение и сохранение данных профиля
+userForm.addEventListener('submit', evt => {
   evt.preventDefault();
-  profileName.textContent = popupUserName.value;
-  profileDescription.textContent = popupActivity.value;
-  popupOpenClose();
-}
 
-popupContainer.addEventListener('submit', popupContainerSubmitHandler);
+  profileName.textContent = username.value;
+  profileDescription.textContent = activity.value;
+
+  editProfile.classList.remove('popup_opened');
+});
+
+//Добавление собственной фотографии места
+placeForm.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  let cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.cards__title').textContent = placeName.value;
+  cardElement.querySelector('.cards__image').src = pictureLink.value;
+
+  cardsList.prepend(cardElement);
+
+  addPhoto.classList.remove('popup_opened');
+});
